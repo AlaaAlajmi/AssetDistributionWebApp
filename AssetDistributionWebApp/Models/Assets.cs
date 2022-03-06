e5333e2f7414dcd2cb3522a9c14e146c5a46b122
AlaaAlajmi <9223@kfh.com>
2022-03-06 11:02:19 +0300

    models

---
+++ AssetDistributionWebApp/Models/Assets.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssetDistributionWebApp.Models
+{
+	public class Assets
+	{
+		public string Name { get; set; }
+		public int Amount { get; set; }
+		public string ImageUrl { get; set; }
+		public string FocalPoint { get; set; }
+	}
+}
