b14f8eebef6d985e0e0388b5c2126f4b145a8af1
Tareq Alsubaie <9228@kfh.com>
2022-03-09 15:03:00 +0300

    user roles added

--- AssetDistributionWebApp/Controllers/HomeController.cs
+++ AssetDistributionWebApp/Controllers/HomeController.cs
@@ -10,44 +10,44 @@ using Microsoft.Extensions.Logging;
 
 namespace AssetDistributionWebApp.Controllers
 {
-	public class HomeController : Controller
-	{
+    public class HomeController : Controller
+    {
         private readonly AppDbContext appDbContext;
         public HomeController(AppDbContext context)
         {
             appDbContext = context;
 
         }
-  //      private readonly ILogger<HomeController> _logger;
-		
-		//public HomeController(ILogger<HomeController> logger)
-		//{
-		//	_logger = logger;
-		//}
+        //      private readonly ILogger<HomeController> _logger;
 
-		public IActionResult Index()
-		{
-			//var data = appDbContext.assets.ToList();
-			return View();
-		}
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //	_logger = logger;
+        //}
 
-		public IActionResult Privacy()
-		{
-			return View();
-		}
-		public IActionResult AreaLeaderHP()
-		{
-			return View();
-		}
-		public IActionResult ApprovalPage()
-		{
-			return View();
-		}
+        public IActionResult Index()
+        {
+            //var data = appDbContext.assets.ToList();
+            return View();
+        }
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult AreaLeaderHP()
+        {
+            return View();
+        }
+        public IActionResult ApprovalPage()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
 }
---
+++ AssetDistributionWebApp/Data/Static/UserRoles.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssetDistributionWebApp.Data.Static
+{
+    public class UserRoles
+    {
+        public const string Sender = "Sender";
+        public const string Reciever = "Reciever";
+    }
+}
